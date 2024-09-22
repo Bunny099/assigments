6347adc2dcf26aa3897ad810990b93c90523edc9
Bunny099 <jayeshkhuman121@gmail.com>
2024-09-23 01:07:26 +0530

    background color changer

---
+++ assignments-main/week-3/easy/bg-color-changer/index.js
@@ -0,0 +1,25 @@
+
+let backGround = document.getElementsByClassName("back-ground").value;
+
+
+function changeRedFunc() {
+    document.body.style.backgroundColor = "red";
+}
+function changeBlueFunc() {
+    document.body.style.backgroundColor = "blue"
+}
+function changeGreenFunc() {
+    document.body.style.backgroundColor = "green"
+}
+function changeBlackFunc() {
+    document.body.style.backgroundColor = "black"
+}
+function changeYellowFunc() {
+    document.body.style.backgroundColor = "yellow"
+}
+function changeWhiteFunc() {
+    document.body.style.backgroundColor = "white"
+}
+// function customFun(){
+//     document.body.style.backgroundColor = input.value;
+// }
